ce353bb219296b969cecbd21b3351e4f672c7eb1
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-05-31 15:23:12 +0100

    finnished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <App />
+    {/* <h1>0</h1>
+    <button>+</button> */}
   </div>,
   document.getElementById("root")
 );
