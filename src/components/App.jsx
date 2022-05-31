1f2de34f31bfb21e90ecf909c67957f21d34ae3a
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-05-31 14:23:26 +0100

    halfway

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const state = useState(123);
+  console.log(state);
+
+  function increase() {
+    // count++;
+  }
+
+  return (
+    <div className="container">
+      <h1>0</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
